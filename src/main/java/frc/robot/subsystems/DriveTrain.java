ca2cf1a7e13f88cf1bf8720fbeb2b04cfaaf565a
Kevin Li <liqiyu1206@gmail.com>
2022-01-21 18:51:54 -0800

    update CAN

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,37 +4,33 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+// import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+// import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
-  PWMSparkMax leftFront;
-  PWMSparkMax rightFront;
-  PWMSparkMax leftBack;
-  PWMSparkMax rightBack;
-  MotorControllerGroup leftMotors;
-  MotorControllerGroup rightMotors;
-  DifferentialDrive drive;
+  private CANSparkMax leftFront = new CANSparkMax(3, MotorType.kBrushless);
+  private CANSparkMax leftBack = new CANSparkMax(4, MotorType.kBrushless);
+  private CANSparkMax rightFront = new CANSparkMax(1, MotorType.kBrushless);
+  private CANSparkMax rightBack = new CANSparkMax(2, MotorType.kBrushless);
+
+  private RelativeEncoder encoder = leftFront.getEncoder();
+  private final DifferentialDrive drive;
 
 
   /** Creates a new DriveTrain. */
   public DriveTrain() {
-    leftFront = new PWMSparkMax(Constants.LEFT_FRONT);
+    leftBack.follow(leftFront);
+    rightBack.follow(rightFront);
     leftFront.setInverted(false);
-    rightFront = new PWMSparkMax(Constants.RIGHT_FRONT);
     rightFront.setInverted(true);
-
-    // leftBack = new PWMSparkMax(Constants.LEFT_BACK)
-    // leftBack.setInverted(false);
-    // rightBack = new PWMSparkMax(Constants.RIGHT_BACK);
-    // rightBack.setInverted(false);
-    // leftMotors = new MotorControllerGroup(leftFront, leftBack);
-    // rightMotors = new MotorControllerGroup(rightFront, rightBack);
-        // NOTE: test the motors one by one
 
     drive = new DifferentialDrive(leftFront, rightFront);
   }
@@ -52,21 +48,20 @@ public class DriveTrain extends SubsystemBase {
 
   public void driveWithTriggers (XboxController controller, double speed){
     // drive.arcadeDrive(controller.getRawAxis(Constants.X_BOX_Y_AXIS)*speed, controller.getRawAxis(Constants.X_BOX_X_AXIS)*speed);
-    drive.arcadeDrive((controller.getRightTriggerAxis() - controller.getLeftTriggerAxis()) * speed, controller.getLeftX()*speed);
+    drive.arcadeDrive(-(controller.getRightTriggerAxis() - controller.getLeftTriggerAxis()) * speed, controller.getLeftX()*speed);
 
   }
 
-  public void Turning (XboxController controller, double speed){
-    // drive.arcadeDrive(0, controller.getRawAxis(Constants.X_BOX_X_AXIS)*speed);
-    drive.arcadeDrive(0, controller.getLeftX()*speed);
-
-  }
-
-  public void driveForward(double speed){
+  public void driveForward (double speed){
     drive.tankDrive(speed, speed);
   }
 
   public void stop(){
     drive.stopMotor();
+  }
+
+  public double getMetersDistance(){
+    // System.out.println(encoder.getPosition() / Constants.GEAR_RATIO * 0.1524 * Math.PI);
+    return encoder.getPosition(); //6 inches to meters
   }
 }
