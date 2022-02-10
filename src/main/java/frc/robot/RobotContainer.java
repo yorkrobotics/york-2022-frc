429eddbd3b0562feec64078a09c7e5e2ac944df6
Kevin Li <liqiyu1206@gmail.com>
2022-02-10 15:03:03 -0800

    merge GearShift subsystem to DriveTrain

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,14 +9,12 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import frc.robot.commands.DriveTeleop;
 import frc.robot.commands.DriveWithPositionControl;
-import frc.robot.commands.GearShiftDown;
-import frc.robot.commands.GearShiftUp;
 import frc.robot.commands.ShootBall;
 import frc.robot.commands.SwitchDriveMode;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.GearShift;
 import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
@@ -28,15 +26,12 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class RobotContainer {
   // Subsystems
   private DriveTrain mDrive;
-  private GearShift mGearShift;
   private Shooter mShooter;
 
   // Commands
   private DriveTeleop driveTeleop;
   private DriveWithPositionControl driveWithPositionControl;
   private SwitchDriveMode switchDriveMode;
-  private GearShiftDown gearShiftDown;
-  private GearShiftUp gearShiftUp;
   private ShootBall shootBall;
 
   // XboxController
@@ -56,11 +51,6 @@ public class RobotContainer {
     driveWithPositionControl = new DriveWithPositionControl(mDrive);
     switchDriveMode = new SwitchDriveMode(mDrive);
 
-    // GearShift subsystem
-    mGearShift = new GearShift();
-    gearShiftDown = new GearShiftDown(mGearShift, mDrive);
-    gearShiftUp = new GearShiftUp(mGearShift, mDrive);
-
     // Shooter subsystem
     mShooter = new Shooter();
     shootBall = new ShootBall(mShooter);
@@ -79,10 +69,12 @@ public class RobotContainer {
   private void configureButtonBindings() {  
     JoystickButton button_A = new JoystickButton(mController, Button.kA.value);
     button_A.whenPressed(driveWithPositionControl);
-    JoystickButton button_B = new JoystickButton(mController, Button.kB.value);
-    button_B.whenPressed(gearShiftDown);
-    JoystickButton button_Y = new JoystickButton(mController, Button.kY.value);
-    button_Y.whenPressed(gearShiftUp);
+
+    JoystickButton right_bumper = new JoystickButton(mController, Button.kRightBumper.value);
+    right_bumper.whenPressed(new InstantCommand(mDrive::shiftUp, mDrive));
+    JoystickButton left_bumper = new JoystickButton(mController, Button.kLeftBumper.value);
+    left_bumper.whenPressed(new InstantCommand(mDrive::shiftDown, mDrive));
+
     JoystickButton button_X = new JoystickButton(mController, Button.kX.value);
     button_X.whenPressed(switchDriveMode);
   }
