bfdbeb6f687f7fa071cf011ff8bbe4111290059d
Kevin Li <liqiyu1206@gmail.com>
2022-03-12 10:04:56 -0800

    add new autonomous routine

--- src/main/java/frc/robot/autonomous/CommandBuilder.java
+++ src/main/java/frc/robot/autonomous/CommandBuilder.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import frc.robot.autonomous.routines.DoNothing;
 import frc.robot.autonomous.routines.DriveForwardBackward2m;
+import frc.robot.autonomous.routines.FiveBalls;
 import frc.robot.autonomous.routines.ThreeBalls231;
 import frc.robot.autonomous.routines.TwoBalls2To3;
 
@@ -24,6 +25,7 @@ public class CommandBuilder {
         mAutoRoutines.add(new DriveForwardBackward2m());
         mAutoRoutines.add(new TwoBalls2To3());
         mAutoRoutines.add(new ThreeBalls231());
+        mAutoRoutines.add(new FiveBalls());
 
     }
 
