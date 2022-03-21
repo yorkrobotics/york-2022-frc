981b17a6acbe3ef146c270ea0c8bd6d152c74701
Kevin Li <liqiyu1206@gmail.com>
2022-03-21 16:29:24 -0700

    Cleaned up auto paths and commands

--- src/main/java/frc/robot/autonomous/CommandBuilder.java
+++ src/main/java/frc/robot/autonomous/CommandBuilder.java
@@ -5,7 +5,6 @@ import java.util.Set;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import frc.robot.autonomous.routines.DoNothing;
-import frc.robot.autonomous.routines.DriveForwardBackward2m;
 import frc.robot.autonomous.routines.FiveBalls;
 import frc.robot.autonomous.routines.ThreeBalls231;
 import frc.robot.autonomous.routines.TwoBalls2To3;
@@ -22,7 +21,6 @@ public class CommandBuilder {
 
     public CommandBuilder(){
 
-        mAutoRoutines.add(new DriveForwardBackward2m());
         mAutoRoutines.add(new TwoBalls2To3());
         mAutoRoutines.add(new ThreeBalls231());
         mAutoRoutines.add(new FiveBalls());
