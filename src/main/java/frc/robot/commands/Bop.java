aed4cdaefa10d650980cafe05b147178d25943dd
wedoalittlecoding <cordierw2024@york.org>
2022-03-31 19:09:25 -0700

    Added burp & bop

---
+++ src/main/java/frc/robot/commands/Bop.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Constants;
+import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.Intake;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Bop extends InstantCommand {
+  private Intake mIntake = Intake.getInstance();
+  private Conveyor mConveyor = Conveyor.getInstance();
+  public Bop() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(mIntake, mConveyor);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    if (mIntake.isDeployed()){
+      mIntake.runRoller(-Constants.INTAKE_ROLLER_SPEED);
+    }
+    mConveyor.runConveyor(Constants.CONVEYOR_SPEED);
+  }
+}
